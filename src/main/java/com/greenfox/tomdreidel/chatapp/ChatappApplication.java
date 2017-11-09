835497868d4a1b8892d3cc74a916bb000bf1373c
tomdreidel <redly.tamas@gmail.com>
2017-11-09 16:57:59 +0100

    commandlinerunner deleted

--- src/main/java/com/greenfox/tomdreidel/chatapp/ChatappApplication.java
+++ src/main/java/com/greenfox/tomdreidel/chatapp/ChatappApplication.java
@@ -1,26 +1,12 @@
 package com.greenfox.tomdreidel.chatapp;
 
-import com.greenfox.tomdreidel.chatapp.model.LogItem;
-import com.greenfox.tomdreidel.chatapp.repository.LogRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatappApplication implements CommandLineRunner {
-
-	@Autowired
-  LogRepository logRepository;
+public class ChatappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatappApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		logRepository.save(new LogItem());
-		logRepository.save(new LogItem());
-		logRepository.save(new LogItem());
 	}
 }
