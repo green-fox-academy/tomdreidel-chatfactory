1bae5d4c922b42f14d44cac92cb9a64b0af75047
tomdreidel <redly.tamas@gmail.com>
2017-11-10 14:53:55 +0100

    not working, need to check

--- src/main/java/com/greenfox/tomdreidel/chatapp/controller/Rest.java
+++ src/main/java/com/greenfox/tomdreidel/chatapp/controller/Rest.java
@@ -1,9 +1,16 @@
 package com.greenfox.tomdreidel.chatapp.controller;
 
 import com.greenfox.tomdreidel.chatapp.model.ChatUser;
+import com.greenfox.tomdreidel.chatapp.model.Todo;
 import com.greenfox.tomdreidel.chatapp.repository.UserRepository;
 import com.greenfox.tomdreidel.chatapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,42 +30,49 @@ public class Rest {
   UserService userService;
 
   @RequestMapping(value = "/users/{id}/send")
-  public String sendUser(@PathVariable long id) {
+  public @ResponseBody
+  ResponseEntity<String> sendUser(@PathVariable long id) {
 
-//    RestTemplate sendUser = new RestTemplate();
-//
-//    HttpHeaders header = new HttpHeaders();
+    RestTemplate sendUser = new RestTemplate();
+
+    HttpHeaders header = new HttpHeaders();
 //    header.set("Host", "localhost:8080");
-//    header.set("Content-Type", "application/json");
-//    header.set("Cache-Control", "no-cache");
-//
-//    MultiValueMap<String, String> content = new LinkedMultiValueMap();
-//    content.add("userName", userRepository.findOne(id).getUserName().toUpperCase());
-//
-//    HttpEntity<MultiValueMap<String, String>> builtRequest = new HttpEntity<>(content, header);
-//
-//    ResponseEntity<String> message = sendUser
-//        .exchange("http://localhost:8080/users/add/rest", HttpMethod.POST, builtRequest, String.class);
+    header.set("Content-Type", "application/json");
+    header.set("Cache-Control", "no-cache");
+
+    MultiValueMap<String, String> content = new LinkedMultiValueMap();
+    content.add("title", userRepository.findOne(id).getUserName().toUpperCase());
+
+    HttpEntity<MultiValueMap<String, String>> builtRequest = new HttpEntity<>(content, header);
+
+    ResponseEntity<String> message = sendUser
+        .exchange("https://tocan.herokuapp.com/add", HttpMethod.POST, builtRequest, String.class);
+
 
 
 //    final String uri = "http://localhost:8080/users/add/rest";
 
-    ChatUser newUser = new ChatUser(userRepository.findOne(id).getUserName().toUpperCase());
+//    ChatUser newUser = new ChatUser(userRepository.findOne(id).getUserName().toUpperCase());
 
-    addRestUser(newUser);
-    return "redirect:/users";
+//    addRestUser(newUser);
+//    addRestTodo(new Todo(userRepository.findOne(id).getUserName()));
+    return message;
 
 //    RestTemplate restTemplate = new RestTemplate();
 //    ChatUser result = restTemplate.postForObject( uri, newUser, ChatUser.class);
 //    return result;
 
-
   }
 
-  @PostMapping("/users/add/rest")
-  public String addRestUser(@RequestBody ChatUser user) {
-    userService.addUser(user);
-    return "redirect:/users";
-  }
+//  @PostMapping("/users/add/rest")
+//  public String addRestUser(@RequestBody ChatUser user) {
+//    userService.addUser(user);
+//    return "redirect:/users";
+//  }
+//
+//  @PostMapping("https://tocan.herokuapp.com/add")
+//  public String addRestTodo(@RequestBody Todo todo) {
+//    return "redirect:/users";
+//  }
 
 }
---
+++ src/main/java/com/greenfox/tomdreidel/chatapp/model/Todo.java
@@ -0,0 +1,10 @@
+package com.greenfox.tomdreidel.chatapp.model;
+
+public class Todo {
+
+  private String title;
+
+  public Todo(String title) {
+    this.title = title;
+  }
+}
